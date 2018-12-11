8cdebe13bcf1de2c41e3374d43e812a591696047
Ryan Norfield <rnorfield@gmail.com>
2018-12-11 12:24:13 -0800

    image gallery working

--- public/js/script.js
+++ public/js/script.js
@@ -28,6 +28,17 @@ function getWeather(){
 	})).responseJSON;
 };
 
+function getPics(keyword) {
+	return ($.ajax({
+		url: `https://pixabay.com/api/?key=10969109-00182948b54a05ec6dd1b7028&q=`+keyword+`&image_type=photo&pretty=true`,
+		dataType: 'json',
+		async: false,
+		success: function(data) {
+			return data;
+		}
+	})).responseJSON;
+}
+
 document.getElementById('weatherbtn').addEventListener("click", function(){
 	data = getWeather()
 	console.log(data)
@@ -36,4 +47,24 @@ document.getElementById('weatherbtn').addEventListener("click", function(){
 	console.log(result,resimg)
 	document.getElementById('resultimg').src = '/img/' + resimg + '.png';
 	document.getElementById('summary').innerHTML = result;
+});
+
+document.getElementById('gallerybtn').addEventListener("click", function(){
+	document.getElementById('images').innerHTML = '';
+	var search = document.getElementById('gallerytext').value;
+	console.log(search)
+    var tempData = getPics(search);
+    console.log(tempData)
+    for (var i = 0; i < 10; i++) {
+    	console.log(tempData.hits[i])
+    	link = tempData.hits[i].webformatURL;
+    	var div = document.createElement("DIV");
+    	div.setAttribute("id", "displayImg")
+	    // image 
+	    var img = document.createElement("img");
+	    img.setAttribute("id", "displayImg");
+	    img.setAttribute("src", link);
+	    div.appendChild(img);
+	    document.getElementById("images").appendChild(div);
+    }
 });
